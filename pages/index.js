37728cb1617174fa8a18945a52e7a2f9bb311a22
zmertz <zackery.mertz@gmail.com>
2023-02-05 22:05:14 -0800

    Removing some lines

--- pages/index.js
+++ pages/index.js
@@ -16,9 +16,6 @@ import design from '../public/design.png';
 import web1 from "../public/web1.png";
 import web2 from "../public/web2.png";
 import web3 from "../public/web3.png";
-import web4 from "../public/web4.png";
-import web5 from "../public/web5.png";
-import web6 from "../public/web6.png";
 
 export default function Home() {
   const [darkMode, setDarkMode] = useState(false);
